7b7b6ae266c9e3d522e562e03cb57b7fbec6ab56
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-24 23:44:16 +0100

    all characters endpoint exceptions

--- src/main/kotlin/com/waleska404/routes/AllCharacters.kt
+++ src/main/kotlin/com/waleska404/routes/AllCharacters.kt
@@ -1,7 +1,29 @@
 package com.waleska404.routes
 
+import com.waleska404.models.ApiResponse
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
 
 fun Route.getAllCharacters() {
-
+    get("/shrek/characters") {
+        try {
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            require(page in 1..5)
+            call.respond(message = page)
+        } catch (e: NumberFormatException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Only Numbers Allowed."),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Characters not Found."),
+                status = HttpStatusCode.BadRequest
+            )
+        }
+    }
 }
