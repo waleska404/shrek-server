d3c7bda0ba926dac551493e45d3e88e0325ec369
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-24 22:44:12 +0100

    Initial commit

---
+++ src/main/kotlin/com/waleska404/Application.kt
@@ -0,0 +1,14 @@
+package com.waleska404
+
+import com.waleska404.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
