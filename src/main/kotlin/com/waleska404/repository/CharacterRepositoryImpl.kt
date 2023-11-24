8e334f021533bb9b878dafae6a14b8cd3cfab82d
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-24 23:09:21 +0100

    repository interface

---
+++ src/main/kotlin/com/waleska404/repository/CharacterRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.waleska404.repository
+
+import com.waleska404.models.ApiResponse
+import com.waleska404.models.Character
+
+class CharacterRepositoryImpl : CharactersRepository {
+    override val characters: Map<Int, List<Character>>
+        get() = TODO("Not yet implemented")
+    override val page1: List<Character>
+        get() = TODO("Not yet implemented")
+    override val page2: List<Character>
+        get() = TODO("Not yet implemented")
+    override val page3: List<Character>
+        get() = TODO("Not yet implemented")
+    override val page4: List<Character>
+        get() = TODO("Not yet implemented")
+    override val page5: List<Character>
+        get() = TODO("Not yet implemented")
+
+    override suspend fun getAllCharacters(page: Int): ApiResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchCharacters(name: String): ApiResponse {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/com/waleska404/repository/CharactersRepository.kt
@@ -0,0 +1,18 @@
+package com.waleska404.repository
+
+import com.waleska404.models.ApiResponse
+import com.waleska404.models.Character
+
+interface CharactersRepository {
+
+    val characters: Map<Int, List<Character>>
+
+    val page1: List<Character>
+    val page2: List<Character>
+    val page3: List<Character>
+    val page4: List<Character>
+    val page5: List<Character>
+
+    suspend fun getAllCharacters(page: Int = 1): ApiResponse
+    suspend fun searchCharacters(name: String): ApiResponse
+}
