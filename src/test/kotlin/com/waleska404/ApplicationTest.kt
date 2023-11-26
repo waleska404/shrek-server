19909d65021f0907216e9f2040d2e69e3f7f856a
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-26 12:00:02 +0000

    characters endpoint test

--- src/test/kotlin/com/waleska404/ApplicationTest.kt
+++ src/test/kotlin/com/waleska404/ApplicationTest.kt
@@ -9,14 +9,29 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlinx.serialization.json.Json
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.java.KoinJavaComponent.inject
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     private val characterRepository: CharacterRepository by inject(CharacterRepository::class.java)
+
+    @Before
+    fun setup() {
+        startKoin{
+            modules(koinModule)
+        }
+    }
+
+    @After
+    fun close() {
+        stopKoin()
+    }
 
     @Test
     fun `access root endpoint, assert correct information`() = testApplication {
@@ -33,9 +48,6 @@ class ApplicationTest {
 
     @Test
     fun `access all heroes endpoint, assert correct information`() = testApplication {
-        startKoin{
-            modules(koinModule)
-        }
         val response = client.get("/shrek/characters")
         assertEquals(
             expected = HttpStatusCode.OK,
@@ -51,4 +63,57 @@ class ApplicationTest {
         val actual = Json.decodeFromString<ApiResponse>(response.body())
         assertEquals(expected = expected, actual = actual)
     }
+
+    @Test
+    fun `access all heroes endpoint, query second page, assert correct information`() = testApplication {
+        val response = client.get("/shrek/characters?page=2")
+        assertEquals(
+            expected = HttpStatusCode.OK,
+            actual = response.status
+        )
+        val expected = ApiResponse(
+            success = true,
+            message = "OK",
+            prevPage = 1,
+            nextPage = 3,
+            characters = characterRepository.page2
+        )
+        val actual = Json.decodeFromString<ApiResponse>(response.body())
+        assertEquals(expected = expected, actual = actual)
+    }
+
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() =
+        testApplication {
+            val pages = 1..5
+            val characters = listOf(
+                characterRepository.page1,
+                characterRepository.page2,
+                characterRepository.page3,
+                characterRepository.page4,
+                characterRepository.page5
+            )
+            pages.forEach { page ->
+                val response = client.get("/shrek/characters?page=$page")
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.bodyAsText())
+                val expected = ApiResponse(
+                    success = true,
+                    message = "OK",
+                    prevPage = calculatePrevPage(page = page),
+                    nextPage = calculateNextPage(page = page),
+                    characters = characters[page - 1],
+                )
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+
+    private fun calculateNextPage(page: Int) = if (page in 1..4) page + 1 else null
+    private fun calculatePrevPage(page: Int) = if (page in 2..5) page - 1 else null
 }
